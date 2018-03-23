0252e484b36afe78164999feb538c860e00df6d2
realnot16 <benedetto.padula@studenti.polito.it>
2018-03-23 14:19:28 +0100

    Merge branch 'refactoring_mvc' of https://github.com/TdP-2018/IndovinaNumero.git into Refactoring
    Conflicts:
    	src/it/polito/tdp/indonumero/IndoNumero.fxml
    	src/it/polito/tdp/indonumero/IndoNumeroController.java
    	src/it/polito/tdp/indonumero/Main.java

--- src/it/polito/tdp/indonumero/IndoNumeroController.java
+++ src/it/polito/tdp/indonumero/IndoNumeroController.java
@@ -1,125 +1,132 @@
+/**
+ * Controller per "Indovina il numero".
+ * 
+ * Delega gli aspetti algoritmici alla classe {@link Model}.
+ * Utilizza i <em>bindings<em> e le <em>property</em> per automatizzare l'aggiornamento dell'interfaccia
+ */
 
 package it.polito.tdp.indonumero;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.HBox;
 
 public class IndoNumeroController {
-	Partita p;
-
-    @FXML
+	
+	private Model model ;
+	
+    @FXML // ResourceBundle that was given to the FXMLLoader
     private ResourceBundle resources;
 
-    @FXML
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
     private URL location;
 
-    @FXML
-    private HBox boxNew;
-    
-    @FXML
-    private Button btnNew;
+    @FXML // fx:id="btnNuova"
+    private Button btnNuova; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtCurr"
+    private TextField txtCurr; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtMax"
+    private TextField txtMax; // Value injected by FXMLLoader
+
+    @FXML // fx:id="boxGioco"
+    private HBox boxGioco; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtTentativo"
+    private TextField txtTentativo; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtLog"
+    private TextArea txtLog; // Value injected by FXMLLoader
 
     @FXML
-    private ChoiceBox<String> choiceDiff;
-
-    @FXML
-    private HBox boxTent;
-
-    @FXML
-    private TextField txtProva;
-    
-    @FXML
-    private ProgressBar barTent;
-    
-    @FXML
-    private CheckBox checkAssistita;
-
-
-    @FXML
-    private TextArea txtLog;
-
-    @FXML
-    void HandleNew(ActionEvent event) {
-    	txtLog.clear();
-    	if(choiceDiff.getValue()==null) {
-    		txtLog.appendText("Devi Inserire una difficoltà!\n");
-    		return;
-    	}
-    	this.newGame();
-    	if(checkAssistita.isSelected())
-    		p= new PartitaAssistita(choiceDiff.getValue());
-    	else
-    		p = new Partita(choiceDiff.getValue());
-    	txtLog.appendText(p.newPart(choiceDiff.getValue()));
+    void handleNuova(ActionEvent event) {
     	
+    	model.newGame() ;
+    	
+    	txtLog.clear() ;
+    	txtTentativo.clear();
+    	
+    	txtLog.setText(String.format("Indovina un numero tra %d e %d\n",
+    			1, model.getNMAX()));
+
     }
 
     @FXML
-    void HandleProva(ActionEvent event) {
-    	int tent=0;
+    void handleProva(ActionEvent event) {
     	
+    	String numS = txtTentativo.getText() ;
+    	
+    	if(numS.length()==0) {
+    		txtLog.appendText("Devi inserire un numero\n");
+    		return ;
+    	}
+
     	try {
-    			tent = Integer.parseInt(txtProva.getText());
+    		int num = Integer.parseInt(numS) ;
+    		// numero era effettivamente un intero
+    		
+    		if(!model.valoreValido(num)) {
+    			txtLog.appendText("Valore fuori dall'intervallo consentito\n");
+    			return ;
+    		}
+    		
+    		int risultato = model.tentativo(num) ;
+    		
+    		if(risultato==0) {
+    			// ha indovinato
+    			txtLog.appendText("Hai vinto!\n");
+    		} else if(risultato<0) {
+    			txtLog.appendText("Troppo basso\n");
+    		} else {
+    			txtLog.appendText("Troppo alto\n");
+    		}
+    		
+    		if(!model.isInGame()) {
+    			// la partita è finita (vittoria o sconfitta)
+    			
+    			if(risultato!=0) {
+        			txtLog.appendText("Hai perso!\n");
+        			txtLog.appendText(
+        					String.format("Il numero segreto era: %d\n",
+        							model.getSegreto())) ;
+    			}
+    		}
+    		    		
+    	} catch(NumberFormatException ex) {
+    		txtLog.appendText("Il dato inserito non è numerico\n");
+    		return ;
     	}
-    	catch(NumberFormatException ex) {
-    		txtLog.appendText("Formato non valido\n");
-    		return;
-    	}
-    	if(tent<1||tent>p.getNmax()) {
-    		txtLog.appendText("Inserire un numero consentito.\n");
-    		return;
-    	}
-    	String result= p.confronta(tent);
-    	if(result.compareTo("Indovinato!")==0)
-    		this.reset();
-    	else if(result.compareTo("Hai perso, troppi tentativi.\n")==0)
-    		this.reset();
     	
-    	barTent.setProgress((double)p.getTcorr()/p.getTmax());  
-    	txtLog.appendText(result+"\n");
-
     }
 
-    @FXML
-    void HandleStop(ActionEvent event) {
-    	this.reset();
-    }
-
-    @FXML
+    @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
-        assert boxNew != null : "fx:id=\"boxNew\" was not injected: check your FXML file 'IndoNumero.fxml'.";
-        assert choiceDiff != null : "fx:id=\"choiceDiff\" was not injected: check your FXML file 'IndoNumero.fxml'.";
-        assert btnNew != null : "fx:id=\"btnNew\" was not injected: check your FXML file 'IndoNumero.fxml'.";
-        assert boxTent != null : "fx:id=\"boxTent\" was not injected: check your FXML file 'IndoNumero.fxml'.";
-        assert txtProva != null : "fx:id=\"txtProva\" was not injected: check your FXML file 'IndoNumero.fxml'.";
+        assert btnNuova != null : "fx:id=\"btnNuova\" was not injected: check your FXML file 'IndoNumero.fxml'.";
+        assert txtCurr != null : "fx:id=\"txtCurr\" was not injected: check your FXML file 'IndoNumero.fxml'.";
+        assert txtMax != null : "fx:id=\"txtMax\" was not injected: check your FXML file 'IndoNumero.fxml'.";
+        assert boxGioco != null : "fx:id=\"boxGioco\" was not injected: check your FXML file 'IndoNumero.fxml'.";
+        assert txtTentativo != null : "fx:id=\"txtTentativo\" was not injected: check your FXML file 'IndoNumero.fxml'.";
         assert txtLog != null : "fx:id=\"txtLog\" was not injected: check your FXML file 'IndoNumero.fxml'.";
-        assert checkAssistita != null : "fx:id=\"checkAssistita\" was not injected: check your FXML file 'IndoNumero.fxml'.";
-        assert barTent != null : "fx:id=\"barTent\" was not injected: check your FXML file 'IndoNumero.fxml'.";
-        choiceDiff.getItems().addAll("Facile","Media","Difficile");
 
     }
-    
-    public void newGame() {
-    	boxTent.setDisable(false);
-    	btnNew.setDisable(true);
-    	choiceDiff.setDisable(true);
-    	checkAssistita.setDisable(true);
-    }
-    public void reset() {
-    	boxTent.setDisable(true);
-    	btnNew.setDisable(false);
-    	choiceDiff.setDisable(false);
-    	checkAssistita.setDisable(false);
-    	txtLog.clear();
-    }
+
+	public void setModel(Model model) {
+		this.model = model;
+		
+		// aggiorna automaticamente i tentativi totali e quelli fatti
+		txtCurr.textProperty().bind(Bindings.convert(model.tentativiProperty()));
+		txtMax.textProperty().bind(Bindings.convert(model.TMAXProperty()));
+		
+		// le aree dell'interfaccia si abilitano-disabilitano automaticamente in funzione di inGame
+		btnNuova.disableProperty().bind(model.inGameProperty());
+		boxGioco.disableProperty().bind(Bindings.not(model.inGameProperty()));
+	}
 }
-
